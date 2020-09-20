c558a248d132c76331e5f4da8be32302a3e8618d
Eason Yi <lisliefor@163.com>
2020-09-20 15:49:30 +0800

    完成自动轮播

--- week12/jsx/main.js
+++ week12/jsx/main.js
@@ -10,14 +10,33 @@ class Carousel extends Component {
     }
     render() {
         this.root = document.createElement('div');
+        this.root.classList.add('carousel');
         for (let r of this.attributes.src) {
-            let child = document.createElement('img');
-            child.style.height = '150px';
-            child.style.marginLeft = '15px';
-            child.style.marginTop = '15px';
-            child.src = r;
+            let child = document.createElement('div');
+            child.style.backgroundImage = `url('${r}')`;
             this.root.appendChild(child);
         }
+
+        let currentIndex = 0;
+        setInterval(() => {
+            let children = this.root.children;
+
+            let nextIndex = (currentIndex + 1) % children.length;
+            let current = children[currentIndex];
+            let next = children[nextIndex];
+
+            next.style.transition = 'none';
+            next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+            
+            setTimeout(() => {
+                next.style.transition = '';
+                current.style.transform = `translateX(${-100 - currentIndex * 100}%)`;
+                next.style.transform = `translateX(${- nextIndex * 100}%)`;
+
+                currentIndex = nextIndex;
+            }, 16); // 16ms = 浏览器一帧的时间
+        }, 3000);
+
         return this.root;
     }
     mountTo(parent) {
@@ -26,10 +45,10 @@ class Carousel extends Component {
 }
 
 let d = [
-    '../images/1.jpg',
-    '../images/2.jpg',
-    '../images/3.jpg',
-    '../images/4.jpg',
+    './images/1.jpg',
+    './images/2.jpg',
+    './images/3.jpg',
+    './images/4.jpg',
 ];
 
 let a = <Carousel src={d} />;
