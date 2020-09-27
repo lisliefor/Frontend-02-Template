b7cb35eaddf00587e19e55bb86e4895a50fcd5aa
Eason Yi <lisliefor@163.com>
2020-09-27 22:05:04 +0800

    完成第四节课练习

--- week13/carousel/animation-demo.js
+++ week13/carousel/animation-demo.js
@@ -1,9 +1,13 @@
 import { Timeline, Animation } from './animation.js';
+import { ease, easeIn, easeOut, easeInOut } from './ease.js';
 
 let t1 = new Timeline();
 t1.start();
 
-t1.add(new Animation(document.querySelector('#el').style, 'transform', 0, 500, 2000, 0, null, v => `translateX(${v}px)`));
+t1.add(new Animation(document.querySelector('#el').style, 'transform', 0, 500, 2000, 0, easeIn, v => `translateX(${v}px)`));
+
+document.querySelector('#el2').style.transition = 'transform ease-in 2s';
+document.querySelector('#el2').style.transform = 'translateX(500px)';
 
 document.querySelector('#pause-btn').addEventListener('click', evt => {
     t1.pause();
--- week13/carousel/animation.js
+++ week13/carousel/animation.js
@@ -20,15 +20,16 @@ export class Timeline {
             for (let animation of this[ANIMATIONS]) {
                 let t;
                 if (this[START_TIME].get(animation) < startTime)
-                    t = now - startTime - this[PAUSE_TIME];
+                    t = now - startTime - this[PAUSE_TIME] - animation.delay;
                 else
-                    t = now - this[START_TIME].get(animation) - this[PAUSE_TIME];
+                    t = now - this[START_TIME].get(animation) - this[PAUSE_TIME] - animation.delay;
 
                 if (animation.duration < t) {
                     this[ANIMATIONS].delete(animation);
                     t = animation.duration;
                 }
-                animation.receive(t);
+                if (t > 0)
+                    animation.receive(t);
             }
             this[TICK_HANDLER] = requestAnimationFrame(this[TICK]);
         }
@@ -46,7 +47,15 @@ export class Timeline {
     }
 
     // 重启
-    reset() {}
+    reset() {
+        this.pause();
+        let startTime = Date.now();
+        this[PAUSE_TIME] = 0;
+        this[ANIMATIONS] = new Set();
+        this[START_TIME] = new Map();
+        this[PAUSE_START] = 0;
+        this[TICK_HANDLER] = null;
+    }
 
     add(animation, startTime) {
         if (arguments.length < 2) {
@@ -59,6 +68,8 @@ export class Timeline {
 
 export class Animation {
     constructor(object, property, startValue, endValue, duration, delay, timingFunction, template) {
+        timingFunction = timingFunction || (v => v);
+        template = template || (v => v);
         this.object = object;
         this.property = property;
         this.startValue = startValue;
@@ -70,6 +81,7 @@ export class Animation {
     }
     receive(time) {
         let range = this.endValue - this.startValue;
-        this.object[this.property] = this.template(this.startValue + range * time / this.duration);
+        let progress = this.timingFunction(time / this.duration);
+        this.object[this.property] = this.template(this.startValue + range * progress);
     }
 }
