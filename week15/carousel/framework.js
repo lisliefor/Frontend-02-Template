d626dd381687a70a43da1b38f1c6be6f44c1a9a7
Eason Yi <lisliefor@163.com>
2020-10-18 21:59:25 +0800

    完成Button和List组件

--- week15/carousel/framework.js
+++ week15/carousel/framework.js
@@ -5,10 +5,18 @@ export function createElement(type, attributes, ...children) {
     for (let name in attributes) {
         element.setAttribute(name, attributes[name]);
     }
-    for (let child of children) {
-        if (typeof child === 'string') child = new TextWrapper(child);
-        element.appendChild(child);
+    let processChildren = (children) => {
+        for (let child of children) {
+            if (typeof child === 'object' && child instanceof Array) {
+                processChildren(child);
+                continue;
+            } 
+            if (typeof child === 'string') child = new TextWrapper(child);
+            element.appendChild(child);
+        }
     }
+    processChildren(children);
+    
     return element;
 }
 
@@ -19,6 +27,9 @@ export class Component {
     constructor(type) {
         this[ATTRIBUTE] = Object.create(null);
         this[STATE] = Object.create(null);
+    }
+    render() {
+        return this.root;
     }
     setAttribute(name, value) {
         this[ATTRIBUTE][name] = value;
@@ -39,21 +50,17 @@ export class Component {
 
 class TextWrapper extends Component {
     constructor(content) {
+        super();
         this.root = document.createTextNode(content);
     }
 }
 
 class ElementWrapper extends Component {
     constructor(type) {
+        super();
         this.root = document.createElement(type);
     }
     setAttribute(name, value) {
         this.root.setAttribute(name, value);
-    }
-    appendChild(child) {
-        child.mountTo(this.root);
-    }
-    mountTo(parent) {
-        parent.appendChild(this.root);
     }
 }
--- week15/carousel/main.js
+++ week15/carousel/main.js
@@ -1,25 +1,49 @@
 import { createElement } from './framework';
-import { Carousel } from './carousel.js';
+import { Carousel } from './Carousel.js';
+import { Button } from './Button.js';
+import { List } from './List.js';
 
 let d = [
     {
         img: './images/1.png',
         url: 'https://time.geekbang.org',
+        title: 'image 1',
     },{
         img: './images/2.png',
         url: 'https://time.geekbang.org',
+        title: 'image 2',
     },{
         img: './images/3.png',
         url: 'https://time.geekbang.org',
+        title: 'image 3',
     },{
         img: './images/4.png',
         url: 'https://time.geekbang.org',
+        title: 'image 4',
     }
 ];
-
+/*
 let a = 
 <Carousel src={d}
     onChange={event => console.log(event.detail.position)}
     onClick={event => window.location.href = event.detail.data}
 />;
+*/
+
+/*
+let a = <Button>
+content
+</Button>;
+*/
+
+let a = <List data={d}>
+{(r) =>
+    <div>
+        <img style="height: 100px;" src={r.img} />
+        <a href={r.url}>{r.title}</a>
+    </div>
+}
+</List>;
+
+
 a.mountTo(document.body);
