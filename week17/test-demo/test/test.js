135c8fd15853dffa0e7c096b04957c303e848643
Eason Yi <lisliefor@163.com>
2020-10-31 17:17:25 +0800

    完成第三节课练习

--- week17/test-demo/test/test.js
+++ week17/test-demo/test/test.js
@@ -1,6 +1,7 @@
 var assert = require("assert");
-import { add } from '../add.js';
+import { add, mul } from '../add.js';
 
+// ./node_modules/.bin/nyc ./node_modules/.bin/mocha --require @babel/register
 describe("add function testing", () => {
   it("1 + 2 should be 3.", function () {
     assert.equal(add(1, 2), 3);
@@ -8,5 +9,11 @@ describe("add function testing", () => {
 
   it("-9 + 4 should be 5.", function () {
     assert.equal(add(-9, 4), -5);
+  });
+});
+
+describe("mul function testing", () => {
+  it("-3 * -5 should be 15.", function () {
+    assert.equal(mul(-3, -5), 15);
   });
 });
